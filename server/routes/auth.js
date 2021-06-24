af7ffb31ee14ee50b70974e2e729a93387501312
Muthu <muthuku37@gmail.com>
2021-06-24 18:24:47 +0530

    Small change to auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {register, login, forgotPassword, resetPassword} = require('../controllers/auth');
+const {register, login} = require('../controllers/auth');
 const router = express.Router();
 
 router.post('/register',register);
